eb9722a5eb514bf5b3a5466cdccebd67264ff55e
Jhon Chalco <esteban01212@gmail.com>
2024-01-27 12:35:49 -0500

    codigo de deteccion de ruido

---
+++ Assets/Scripts/MicrophoneManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneManager : MonoBehaviour
+{
+    public int sampleWindow = 64;
+    private AudioClip microphoneClip;
+
+    public float vibrationDuration = 0.5f;
+
+    void Start()
+    {
+        MicrophonetToAudioclip();
+    }
+
+    private void Update()
+    {
+        float ruido = (GetLoundnessFromMicrophone() * 100);
+
+        if (ruido < 0.1f) ruido = 0;
+
+        print(ruido);
+    }
+
+    private void MicrophonetToAudioclip()
+    {
+        string microphoneName = Microphone.devices[0];
+        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+    }
+
+    public float GetLoundnessFromMicrophone()
+    {
+        return GetLoundnessFromAudioclip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
+    }
+
+    public float GetLoundnessFromAudioclip(int clipPosition, AudioClip clip)
+    {
+        int startPosition = clipPosition - sampleWindow;
+
+        if (startPosition < 0) return 0;
+
+        float[] waveData = new float[sampleWindow];
+        clip.GetData(waveData, startPosition);
+
+        float totalLoundness = 0;
+
+        for(int i = 0; i < sampleWindow; i++)
+        {
+            totalLoundness += Mathf.Abs(waveData[i]);
+        }
+
+        return totalLoundness / sampleWindow;
+    }
+}
