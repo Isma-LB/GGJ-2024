7978b237e44964d39882c58dc5613b63a9c06d72
IsmaLB <61549609+Isma-LB@users.noreply.github.com>
2024-01-27 18:52:37 -0500

    Send minijuego

--- Assets/Scripts/ScriptableObjects/MiniJuego.cs
+++ Assets/Scripts/ScriptableObjects/MiniJuego.cs
@@ -3,10 +3,25 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "GGJ-2024/minijuego")]
 public class MiniJuego : ScriptableObject
 {
-    [Range(0,60)]
-    public float time = 0.5f;
+    [Header("Scene")]
     [SerializeField] string sceneName = "";
+    [SerializeField, Range(0, 60)] float miniGameTime = 10f;
+    [SerializeField, Range(0, 3)] float timeReducer = 0.5f;
+
+
+    [Header("Instruction")]
+    [SerializeField, TextArea()] public string instructionText = "";
+    [SerializeField, Range(0,30)] public float instructionTime = 5.0f;
+
 
     public string SceneName { get => sceneName; }
+    public float MiniGameTime { get => miniGameTime - timesPlayed * timeReducer; }
+    public void WasPlayed(){
+        timesPlayed++;
+    }
+    public void Reset(){
+        timesPlayed = 0;
+    }
+    int timesPlayed = 0;
 
 }
